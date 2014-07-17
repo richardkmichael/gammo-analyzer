cd4745303c829c2b7ed382450eb95508853367c8
Richard Michael <rmichael@edgeofthenet.org>
2014-07-16 22:03:35 -0400

    Remaining methods to def_column_alias.

--- inspect.rb
+++ inspect.rb
@@ -10,9 +10,9 @@ DB = Sequel.sqlite f
 class Folder < Sequel::Model
   one_to_many :messages, :key => :FolderId
 
-  def id   ; self.FolderId   ; end # Primary key
-  def name ; self.FolderName ; end
-  def type ; self.FolderType ; end
+  def_column_alias :id,   :FolderId
+  def_column_alias :name, :FolderName
+  def_column_alias :type, :FolderType
 end
 
 class Message < Sequel::Model
@@ -26,33 +26,34 @@ class Message < Sequel::Model
 
   many_to_one :folder, :key => :FolderId
 
-  def folder_id        ; self.FolderId        ; end
-  def run_id           ; self.MessageRunId    ; end # Primary key
-  def migration_id     ; self.MigrationId     ; end
-  def message_key      ; self.MessageKey      ; end # FIXME: HEX, but what is it?
-  def migration_status ; self.MigrationStatus ; end
-  def error_code       ; self.ErrorCode       ; end
+  def_column_alias :run_id,           :MessageRunId
+  def_column_alias :folder_id,        :FolderId
+  def_column_alias :error_code,       :ErrorCode
+  def_column_alias :message_key,      :MessageKey       # FIXME: HEX, but what is it?
+  def_column_alias :migration_id,     :MigrationId
+  def_column_alias :migration_status, :MigrationStatus
 end
 
 class FailedMessage < Message
-  def error         ; self.ErrorDesc           ; end
+  def_column_alias :error, :ErrorDesc
+
   def error_code    ; error.split(':').last    ; end
   def error_message ; error.split(',').first   ; end
   def failed_at     ; Time.at self.FailureTime ; end
 end
 
 class FailedEmailMessage < Message
+  def_column_alias :subject,  :Subject
   def_column_alias :sender,   :SenderEmailAddress
   def_column_alias :receiver, :ReceiverEmailAddress
-  def_column_alias :subject,  :Subject
 
   def sent_at ; Time.at self.SentTime ; end
 end
 
 class FailedCalendarMessage < Message
+  def_column_alias :title,     :Title
   def_column_alias :organizer, :Organizer
   def_column_alias :attendees, :Attendees
-  def_column_alias :title,     :Title
 
   def start_at  ; Time.at self.StartTime ; end
   def end_at    ; Time.at self.EndTime   ; end
@@ -62,28 +63,5 @@ class FailedContactMessage < Message
   def_column_alias :full_name,     :FullName
   def_column_alias :email_address, :EmailAddress
 end
-
-# Simpler than AS::Inflector (no-acronyms), but still enough.
-# class String
-#   def underscore
-#     word = self.dup
-#     word.gsub!(/::/, '/')
-#     word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-#     word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-#     word.tr!("-", "_")
-#     word.downcase!
-#     word
-#   end
-# end
-
-# def underscore(camel_cased_word)
-#   word = camel_cased_word.to_s.gsub('::', '/')
-#   word.gsub!(/(?:([A-Za-z\d])|^)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1}#{$1 && '_'}#{$2.downcase}" }
-#   word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
-#   word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-#   word.tr!("-", "_")
-#   word.downcase!
-#   word
-# end
 
 binding.pry
